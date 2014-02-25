f80d023129742200de9848abbf991dec40960750
JaideepNair <jaideepnr@gmail.com>
2014-02-25 12:00:57 +0530

    Modified the glue file

--- src/main/java/com/client/app/common/CommonFunctions.java
+++ src/main/java/com/client/app/common/CommonFunctions.java
@@ -2,11 +2,36 @@
 
 package com.client.app.common;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 
 
 
 public class CommonFunctions {	
-	 public void installapp() {
+	   WebDriver driver;
+	   
+	public void setUp() throws MalformedURLException, InterruptedException{
+        
+        //Set up desired capabilities and pass the Android app-activity and app-package to Appium
+       DesiredCapabilities capabilities = new DesiredCapabilities();
+       capabilities.setCapability("device","Android");
+       capabilities.setCapability(CapabilityType.BROWSER_NAME, "Android");
+       capabilities.setCapability(CapabilityType.VERSION, "4.2.2");
+       capabilities.setCapability(CapabilityType.PLATFORM, "Windows");
+       capabilities.setCapability("app-package", "com.dmi.notification"); // This is package name of your app (you can get it from apk info app)
+       capabilities.setCapability("app-activity", "com.dmi.notification.view.activity.SplashScreen"); // This is Launcher activity of your app (you can get it from apk info app)
+          //Create RemoteWebDriver instance and connect to the Appium server.
+       driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+       
+   }
+	
+	public void installapp() {
 		  
 			//TO-DO
 		    }
--- src/test/java/com/client/app/login/StepDefinitionSupport.java
+++ src/test/java/com/client/app/login/StepDefinitionSupport.java
@@ -1,5 +1,11 @@
 package com.client.app.login;
 
+import cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(format = { "json-pretty:target/cucumber-report-StepDefinitionSupport.json" }, glue = { "com.client.app.common",
+"com.client.app.pages" }, features = { "src/test/resources/com/client/app/Android/01_LoginToDMI.feature" })
 public class StepDefinitionSupport {
 
 }
