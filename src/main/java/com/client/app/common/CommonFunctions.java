da7de8d4eee4a44e9a9931585bb86345768f6606
JaideepNair <jaideepnr@gmail.com>
2014-02-28 12:40:16 +0530

    Fixed the issue for format specifier

--- src/main/java/com/client/app/common/CommonFunctions.java
+++ src/main/java/com/client/app/common/CommonFunctions.java
@@ -10,9 +10,6 @@ import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-
-
-
 public class CommonFunctions {	
 	public   WebDriver driver;
 	   
@@ -22,10 +19,12 @@ public class CommonFunctions {
        DesiredCapabilities capabilities = new DesiredCapabilities();
        capabilities.setCapability("device","Android");
        capabilities.setCapability(CapabilityType.BROWSER_NAME, "Android");
-       capabilities.setCapability(CapabilityType.VERSION, "4.2.2");
+       capabilities.setCapability(CapabilityType.VERSION, "4.4.2");
        capabilities.setCapability(CapabilityType.PLATFORM, "Windows");
-       capabilities.setCapability("app-package", "com.dmi.notification"); // This is package name of your app (you can get it from apk info app)
-       capabilities.setCapability("app-activity", "com.dmi.notification.view.activity.SplashScreen"); // This is Launcher activity of your app (you can get it from apk info app)
+    // This is package name of your app (you can get it from apk info app)
+       capabilities.setCapability("app-package", "com.dmi.notification"); 
+    // This is Launcher activity of your app (you can get it from apk info app)
+       capabilities.setCapability("app-activity", "com.dmi.notification.view.activity.SplashScreen"); 
           //Create RemoteWebDriver instance and connect to the Appium server.
        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
        //return(driver);
--- src/test/java/com/client/app/login/LoginToDMIStepDefsTest.java
+++ src/test/java/com/client/app/login/LoginToDMIStepDefsTest.java
@@ -1,48 +1,44 @@
 package com.client.app.login;
 import java.net.MalformedURLException;
 
-import org.openqa.selenium.WebDriver;
-
 import com.client.app.common.CommonFunctions;
 import com.client.app.pages.LoginPage;
 
 import cucumber.annotation.en.Given;
 import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
-import cucumber.runtime.PendingException;
 
 public class LoginToDMIStepDefsTest {
-
-    WebDriver driver;
-    public  CommonFunctions cn = new CommonFunctions();
-    public  LoginPage lp = new LoginPage();
-	
+  
+   
+    
 	public LoginToDMIStepDefsTest() throws MalformedURLException, InterruptedException
 	    {
-		    	//TO-DO - class constructor to fetch values from Config.properties
-		 		 cn.setUp();
+		CommonFunctions cn = new CommonFunctions();
+		cn.setUp();
+
 	  }
 
 
 @Given("^I am on the login screen and has no content$")
-public void I_am_on_the_login_screen_and_has_no_content() throws Throwable {
-	
+public void I_am_on_the_login_screen_and_has_no_content() throws MalformedURLException, InterruptedException   {
+	LoginPage lp = new LoginPage();
 	 lp.inputUsername();
-    throw new PendingException();
+	     
 }
 
 @When("^I login using user name and password$")
-public void I_login_using_user_name_and_password() throws Throwable {
+public void I_login_using_user_name_and_password() {
 	
+	LoginPage lp = new LoginPage();
 	lp.inputPasswordField();
-    throw new PendingException();
+    
 }
 
 @Then("^I will be taken to DMI application$")
-public void I_will_be_taken_to_DMI_application() throws Throwable {
-	
+public void I_will_be_taken_to_DMI_application()  {
+	LoginPage lp = new LoginPage();
 	lp.login();
-    throw new PendingException();
 }
 }
        
